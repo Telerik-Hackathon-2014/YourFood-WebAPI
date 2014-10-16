f3ebd5b67c92fbf7eed6cb8b05fc96608d3529f6
Martin Nikolov <flextry@gmail.com>
2014-10-16 17:41:37 +0300

    Telerik Everlive SDK API Added

---
+++ YourFood.EverliveAPI/Contracts/IImageUploader.cs
@@ -0,0 +1,7 @@
+﻿namespace YourFood.EverliveAPI.Contracts
+{
+    public interface IImageUploader
+    {
+        string UrlFromBase64Image(string base64);
+    }
+}
---
+++ YourFood.EverliveAPI/ImageUploader.cs
@@ -0,0 +1,32 @@
+﻿namespace YourFood.EverliveAPI
+{
+    using System;
+    using System.IO;
+    using Telerik.Everlive.Sdk.Core;
+    using Telerik.Everlive.Sdk.Core.Query.Definition.FormData;
+    using YourFood.Common;
+    using YourFood.EverliveAPI.Contracts;
+
+    public class ImageUploader : IImageUploader
+    {
+        private readonly EverliveApp app;
+
+        public ImageUploader()
+            : this(new EverliveApp(ConnectionStrings.EverliveAppKey))
+        {
+        }
+
+        public ImageUploader(EverliveApp everliveApp)
+        {
+            this.app = everliveApp;
+        }
+
+        public string UrlFromBase64Image(string base64)
+        {
+            var stream = new MemoryStream(Convert.FromBase64String(base64));
+            var uploadResult = this.app.WorkWith().Files().Upload(new FileField("Url", Guid.NewGuid().ToString(), "image/jpeg", stream)).ExecuteSync();
+            var url = this.app.WorkWith().Files().GetFileDownloadUrl(uploadResult.Id);
+            return url;
+        }
+    }
+}
