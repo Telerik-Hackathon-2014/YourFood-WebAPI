f49aa4a6b295ced13bc99050d187d896c2858748
Martin Nikolov <flextry@gmail.com>
2014-10-18 02:20:58 +0300

    AvailabilityProductController Updated

---
+++ YourFood.Data/Infrastructure/AspNetUserInfoProvider.cs
@@ -0,0 +1,25 @@
+﻿namespace YourFood.Data.Infrastructure
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using Microsoft.AspNet.Identity;
+
+    public class AspNetUserInfoProvider : IUserInfoProvider
+    {
+        public bool IsUserAuthenticated()
+        {
+            return Thread.CurrentPrincipal.Identity.IsAuthenticated;
+        }
+
+        public string GetUsername()
+        {
+            return Thread.CurrentPrincipal.Identity.GetUserName();
+        }
+
+        public string GetUserId()
+        {
+            return Thread.CurrentPrincipal.Identity.GetUserId();
+        }
+    }
+}
