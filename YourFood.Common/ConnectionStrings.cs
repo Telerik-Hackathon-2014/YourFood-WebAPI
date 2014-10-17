5a08c0bd4e5335be1201c46944586d85d436371d
Martin Nikolov <flextry@gmail.com>
2014-10-17 22:18:49 +0300

    Everlive API Key Changed

--- YourFood.Common/ConnectionStrings.cs
+++ YourFood.Common/ConnectionStrings.cs
@@ -7,6 +7,6 @@
     {
         public const string DefaultConnection = @"Data Source=.\sqlexpress;Initial Catalog=YourFood;Integrated Security=True";
         public const string CloudDatabaseConnection = @"Server=49efb363-8dcb-4c0c-8a29-a3c4014b3d99.sqlserver.sequelizer.com;Database=db49efb3638dcb4c0c8a29a3c4014b3d99;User ID=vdiumcqovosjusyr;Password=cLpSyJuD8Uqvx6rndvFBhcUdgKcXsAHowbUkeHGqVYYRXrk4pEszkDGaZmKWGDmE;";
-        public const string EverliveAppKey = "aXTOUMq6Hy3At5wQ";
+        public const string EverliveAppKey = "Co6BSJivwBpDikcY";
     }
 }
